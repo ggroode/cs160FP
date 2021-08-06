61bfbd019eb220185ee871a94b85ab62dd85d869
Gavin <ggroode@berkeley.edu>
2021-08-06 12:43:24 -0700

    navbar and basic website layout

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("",views.search_recipes,name='index'),
     path("search-recipes",views.search_recipes,name='search-recipes'),
+    path("create-recipe",views.create_recipe,name='create-recipe'),
+    path("edit-recipe/<int:id>",views.create_recipe,name='edit-recipe'),
+    path("recipe/<int:id>",views.recipe,name='recipe'),
+    path("meal/<str:ids>",views.meal,name='meal'),
+    path("help",views.help,name='help'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def search_recipes(request):
     return render(request,'recipe/search_recipes.html')
+def create_recipe(request,id=-1):
+    return render(request,'recipe/create_recipe.html')
+def recipe(request,id):
+    return render(request,'recipe/recipe.html',context={'id':id})
+def meal(request,ids):
+    ids = json.loads(ids)
+    return render(request,'recipe/meal.html',context={'ids':ids})
+def help(request):
+    return render(request,'recipe/help.html')
