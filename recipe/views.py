c24d9d0edb96ed65e31855154eafaf3b4707d398
AnnaLitskevitch <alitskevitch@berkeley.edu>
2021-08-06 20:21:39 -0700

    j

--- recipe/views.py
+++ recipe/views.py
@@ -8,7 +8,8 @@ def search_recipes(request):
 def create_recipe(request,id=-1):
     return render(request,'recipe/create_recipe.html')
 def recipe(request,id):
-    return render(request,'recipe/recipe.html',context={'id':id})
+    recipe = Recipe.objects.filter(id=id)
+    return render(request,'recipe/recipe.html',context={'id':id,'recipe':recipe })
 def meal(request,ids):
     ids = json.loads(ids)
     return render(request,'recipe/meal.html',context={'ids':ids})
