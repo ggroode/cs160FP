a062b8c78c3eae3343c4b2810a4c55e505d21d8e
Aleksander Kozminski <alek.k@berkeley.edu>
2021-08-07 01:18:29 -0700

    update to create-recipes (all html elements added, unstyled)

--- recipe/static/recipe/js/create.js
+++ recipe/static/recipe/js/create.js
@@ -1,3 +1,15 @@
 function addStep() {
-  document.getElementById('steps').innerHTML += "<li><textarea></textarea></li>"
+  document.getElementById('steps').innerHTML += `
+    <li>
+      <textarea></textarea>
+    </li>`;
+}
+
+function addIngredient() {
+  document.getElementById('ingredients').innerHTML += `
+  <li>
+  <input type="text" value="" placeholder="ingredient" class="border">
+  <input type="text" value="" placeholder="amount" class="border">
+  <input type="text" value="" placeholder="unit" class="border">
+  </li>`
 }
