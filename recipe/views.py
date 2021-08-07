940c0cc2a0adb2a3d60c7859cd603ca472cbbfef
AnnaLitskevitch <alitskevitch@berkeley.edu>
2021-08-07 10:03:45 -0700

    yams?

--- recipe/views.py
+++ recipe/views.py
@@ -8,7 +8,7 @@ def search_recipes(request):
 def create_recipe(request,id=-1):
     return render(request,'recipe/create_recipe.html')
 def recipe(request,id):
-    recipe = Recipe.objects.filter(id=id)
+    recipe = Recipe.objects.filter(id=id)[0]
     return render(request,'recipe/recipe.html',context={'id':id,'recipe':recipe })
 def meal(request,ids):
     ids = json.loads(ids)
