57ee52ac857193caf74a245341bb9a6a20e20e7a
Gavin <ggroode@berkeley.edu>
2021-08-08 15:09:18 -0700

    Database issue fix

--- recipe/views.py
+++ recipe/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 def search_recipes(request):
     name = request.GET.get("name","")
     recipes = Recipe.objects.filter(name__contains=name)
-    filters = zip(['author','tag','classification'],['text','text','multi-select'],[(),(),('entree','side')])
+    filters = zip(['visibility','classification','author','tag',],['text','text','multi-select'],[(),(),('entree','side')])
     return render(request,'recipe/search_recipes.html',context={'recipes':recipes,'filters':filters})
 
 @csrf_exempt
